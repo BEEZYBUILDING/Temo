cdbc8cfe95d2d65fc34234fed77efbb6a99b101a
OLAJIRE <olajirefolasore@gmail.com>
2026-03-14 13:59:37 +0100

    feat(products): created the category model
    - created name, slug, description, parent and created_at fields
    - parent is a self referencing FK for category hierarchy
    - overrode save() to auto-generate slug from name

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=100, null=True, blank=True)
+    parent = models.ForeignKey('self', related_name='subcategories', null=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
