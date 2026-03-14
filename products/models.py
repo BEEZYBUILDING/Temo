3d53c8df0ca9e090382305e596e3c956f062d5cc
OLAJIRE <olajirefolasore@gmail.com>
2026-03-14 14:40:06 +0100

    feat(products): created the product model
    - created name, slug, description, category, seller, is_active, created_at, updated_at fields
    - category and seller are FK for the Product hierachy
    - overrode save() to auto-generate slug from name

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from users.models import CustomUser
 
 # Create your models here.
 class Category(models.Model):
@@ -8,6 +9,24 @@ class Category(models.Model):
     description = models.TextField(max_length=100, null=True, blank=True)
     parent = models.ForeignKey('self', related_name='subcategories', null=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    seller = models.ForeignKey(CustomUser, related_name='products', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def save(self, *args, **kwargs):
         if not self.slug:
