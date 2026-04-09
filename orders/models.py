e2da3a683d3ff12ad5c24173f145542e5cab6ff8
OLAJIRE <olajirefolasore@gmail.com>
2026-04-09 07:07:52 +0100

    updated status to choices

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from products.models import ProductVariant
+from users.models import CustomUser
 
 # Create your models here.
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        CONFIRMED = "CONFIRMED", "Confirmed"
+        PROCESSING = "PROCESSING", "Processing"
+        SHIPPED = "SHIPPED", "Shipped"
+        DELIVERED = "DELIVERED", "Delivered"
+        CANCELLED = "CANCELLED", "Cancelled"
+        REFUNDED = "REFUNDED", "Refunded"
+         
+    user = models.ForeignKey(CustomUser, related_name='orders', on_delete=models.CASCADE)
+    address = models.JSONField()
+    status = models.CharField(max_length=15, choices=Status.choices, default=Status.PENDING)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_cost = models.DecimalField(max_digits=4, decimal_places=2)
+    discount_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    #coupon = models.ForeignKey('coupons.Coupon', null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    variant = models.ForeignKey(ProductVariant, related_name='item_variant', on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=20)
+    variant_attributes = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    line_total = models.DecimalField(max_digits=10, decimal_places=2)
+    
