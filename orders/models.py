143d275cefb8d7e82b5d266141d3ece442b2c869
olajire <olajirefolasore@gmail.com>
2026-08-22 20:21:17 +0100

    feat(payments): created Payment model
    - created Payment model with OneToOneField to Order
    - added status choices (pending, succeeded, failed)
    - added stripe_payment_intent_id with unique constraint and index
    - configured created_at and updated_at fields

--- orders/models.py
+++ orders/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2)
     tax_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    coupon = models.ForeignKey(Coupon, related_name='coupon', null=True, blank=True, on_delete=models.SET_NULL)
+    coupon = models.ForeignKey('Coupon', related_name='coupon', null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from orders.models  import Order
 
 # Create your models here.
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        SUCCEEDED = "SUCCEEDED", "Succeeded"
+        FAILED = "FAILED", "Failed" 
+
+    order = models.OneToOneField(Order, related_name='payment', on_delete=models.CASCADE)
+    stripe_payment_intent_id = models.CharField(max_length=255, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=20, default='usd')
+    status = models.CharField(max_length=15, choices=Status.choices, default=Status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['stripe_payment_intent_id'])
+        ]
+    def __str__(self):
+        return f"{self.order} - {self.amount}"
