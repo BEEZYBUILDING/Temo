61f967582a614a8d1faa53c7bd45ba8fdee2d60e
olajire <olajirefolasore@gmail.com>
2026-09-02 20:09:13 +0100

    chore: read ALLOWED_HOSTS from environment

--- temo/settings/base.py
+++ temo/settings/base.py
@@ -27,12 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'plaster-lantern-bovine.ngrok-free.dev',  
-]
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 # Application definition
 
