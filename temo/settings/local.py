aca6fbe364d7cfc2ce9838b782859ca3290f26af
olajire <olajirefolasore@gmail.com>
2026-09-03 07:15:09 +0100

    fix: update wsgi.py to use the correct setting modeule

--- temo/settings/local.py
+++ temo/settings/local.py
@@ -4,6 +4,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'plaster-lantern-bovine.ngrok-free.dev']
 
+DJANGO_SETTINGS_MODULE = temo.settings.base
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://plaster-lantern-bovine.ngrok-free.dev",
